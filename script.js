7d4113bacaeebfbf2893d257c66f4e3a5fb7c10a
Yuna Nomura <nomurayu88@craighead.school.nz>
2024-08-14 22:56:26 +0000

    done

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 function fun() {
-    let userAttempt = prompt('Please enter your choice.')
 
-    let mine = Math.floor(Math.random() * (2 - 0 + 1) + 0)
 
-    if (userAttempt == 'scissors') {
-        if (mine == 0) {
-            alert('I chose scissors. We are draw✌')
+
+
+    let scores = []
+
+    let count = 3
+    while (count > 0) {
+        count = count - 1
+        let userAttempt = prompt('Please enter your choice.')
+        userAttempt = userAttempt.toLocaleLowerCase()
+
+        let mine = Math.floor(Math.random() * (2 - 0 + 1) + 0)
+
+        if (userAttempt == 'scissors') {
+            if (mine == 0) {
+                alert('I chose scissors. We are draw✌') + scores.push("Computer won")
+            }
+            if (mine == 1) {
+                alert('I chose rock. I win✊') + scores.push("Computer won")
+            }
+            if (mine == 2) {
+                alert('I chose paper. You win✌') + scores.push("You won")
+            }
         }
-        if (mine == 1) {
-            alert('I chose rock. I win✊')
+
+        if (userAttempt == 'rock') {
+            if (mine == 0) {
+                alert('I chose scissors. You win✊') + scores.push("You won")
+            }
+            if (mine == 1) {
+                alert('I chose rock. We are draw✊') + scores.push("Computer won")
+            }
+            if (mine == 2) {
+                alert('I chose paper. I won🖐') + scores.push("Computer won")
+            }
         }
-        if (mine == 2) {
-            alert('I chose paper. You win✌')
+
+        if (userAttempt == 'paper') {
+            if (mine == 0) {
+                alert('I chose scissors. I win✌') + scores.push("Computer won")
+            } if (mine == 1) {
+                alert('I chose rock. You win🖐') + scores.push("You won")
+            } if (mine == 2) {
+                alert('I chose paper. We are draw🖐') + scores.push("Computer won")
+            }
         }
     }
 
-    if (userAttempt == 'rock') {
-        if (mine == 0) {
-            alert('I chose scissors. You win✊')
-        }
-        if (mine == 1) {
-            alert('I chose rock. We are draw✊')
-        }
-        if (mine == 2) {
-            alert('I chose paper. I won🖐')
-        }
-    }
+    alert(scores)
 
-    if (userAttempt == 'paper') {
-        if (mine == 0) {
-            alert('I chose scissors. I win✌')
-        } if (mine == 1) {
-            alert('I chose rock. You win🖐')
-        } if (mine == 2) {
-            alert('I chose paper. We are draw🖐')
-        }
-    }
+
+
 }
